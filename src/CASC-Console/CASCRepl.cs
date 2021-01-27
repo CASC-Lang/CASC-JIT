5b5e6297fd78e9b0b1060c922b90cebb1ebb8a19
KyleLin921021 <43753315+KyleLin921021@users.noreply.github.com>
2021-01-27 17:07:45 +0800

    Improve our repl but this version of repl does not support Mandarin :(

---
+++ src/CASC-Console/CASCRepl.cs
@@ -0,0 +1,135 @@
+using CASC.CodeParser.Syntax;
+using CASC.CodeParser;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using CASC.CodeParser.Text;
+
+namespace CASC
+{
+    internal sealed class CASCRepl : Repl
+    {
+        private Compilation _previous;
+        private bool _showTree;
+        private bool _showProgram;
+        private readonly Dictionary<VariableSymbol, object> _variables = new Dictionary<VariableSymbol, object>();
+
+        protected override void RenderLine(string line)
+        {
+            var tokens = SyntaxTree.ParseTokens(line);
+
+            foreach (var token in tokens)
+            {
+                var isKeyword = token.Kind.ToString().EndsWith("Keyword");
+                var isNumber = token.Kind == SyntaxKind.NumberToken;
+
+                if (isKeyword)
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else if (!isNumber)
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                Console.Write(token.Text);
+
+                Console.ResetColor();
+            }
+        }
+
+        protected override void EvalCommand(string input)
+        {
+            switch (input)
+            {
+                case "/showTree":
+                    _showTree = !_showTree;
+                    break;
+                case "/showProgram":
+                    _showProgram = !_showProgram;
+                    break;
+                case "/clear":
+                    Console.Clear();
+                    break;
+                case "/reset":
+                    _previous = null;
+                    _variables.Clear();
+                    break;
+                default:
+                    base.EvalCommand(input);
+                    break;
+            }
+        }
+
+        protected override bool IsComplete(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return true;
+
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            if (syntaxTree.Diagnostics.Any())
+                return false;
+
+            return true;
+        }
+
+        protected override void Eval(string text)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            var compilation = _previous == null
+                                ? new Compilation(syntaxTree)
+                                : _previous.ContinueWith(syntaxTree);
+
+            if (_showTree)
+                syntaxTree.Root.WriteTo(Console.Out);
+
+            if (_showProgram)
+                compilation.EmitTree(Console.Out);
+
+            var result = compilation.Evaluate(_variables);
+
+            if (!result.Diagnostics.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(result.Value);
+                Console.ResetColor();
+                _previous = compilation;
+            }
+            else
+            {
+                foreach (var diagnostic in result.Diagnostics)
+                {
+                    var lineIndex = syntaxTree.Source.GetLineIndex(diagnostic.Span.Start);
+                    var line = syntaxTree.Source.Lines[lineIndex];
+                    var lineNumber = lineIndex + 1;
+                    var character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write($"({lineNumber}, {character}): ");
+                    Console.WriteLine(diagnostic);
+                    Console.ResetColor();
+
+                    var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                    var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                    var prefix = syntaxTree.Source.ToString(prefixSpan);
+                    var error = syntaxTree.Source.ToString(diagnostic.Span);
+                    var suffix = syntaxTree.Source.ToString(suffixSpan);
+
+                    Console.Write("    ");
+                    Console.Write(prefix);
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write(error);
+                    Console.ResetColor();
+
+                    Console.Write(suffix);
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
